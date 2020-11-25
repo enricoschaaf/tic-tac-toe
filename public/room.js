22d22f4220671d591ed8220509d93a46af22a72d
enricoschaaf <enricoschaaf98@gmail.com>
2020-11-25 02:51:56 +0100

    Add favicons and improve messages

--- public/room.js
+++ public/room.js
@@ -1,112 +1,105 @@
-const $ = document.querySelector.bind(document)
-const $$ = document.querySelectorAll.bind(document)
-const room = window.location.pathname.slice(1)
+const $ = document.querySelector.bind(document);
+const $$ = document.querySelectorAll.bind(document);
+const room = window.location.pathname.slice(1);
 
-const main = $("main")
-const youAre = $("#player")
-const overMessage = $("#over-message")
-const loading = $("#loading")
-const currentPlayer = $("#current-player")
-const gameContainer = $("#game-container")
-const noOverElements = $$(".js-no-over")
-const headline = $("#title")
-const buttons = [...Array(9)].map(() => document.createElement("button"))
+const main = $('main');
+const youAre = $('#player');
+const overMessage = $('#over-message');
+const loading = $('#loading');
+const currentPlayer = $('#current-player');
+const gameContainer = $('#game-container');
+const noOverElements = $$('.js-no-over');
+const headline = $('#title');
+const buttons = [...Array(9)].map(() => document.createElement('button'));
 
-document.title += ` (${room})`
+document.title += ` (${room})`;
 
-const socket = io()
+const socket = io();
 
-let player
+let player;
 
-socket.on("connect", () => {
-  socket.emit("JOIN_ROOM", room, () => {
-    loading.textContent = "Waiting for another player... Your ID: "
-    const id = document.createElement("button")
-    id.className = "font-bold cursor-default"
-    id.textContent = room
+socket.on('connect', () => {
+  socket.emit('JOIN_ROOM', room, () => {
+    loading.textContent = 'Waiting for another player... Your ID: ';
+    const id = document.createElement('button');
+    id.className = 'font-bold cursor-default';
+    id.textContent = room;
 
     if (navigator.clipboard) {
-      id.textContent += " (copy)"
-      id.className += " cursor-pointer underline hover:text-blue-400"
-      id.addEventListener("click", () => {
-        const href = new URL(room, window.location).href
-        navigator.clipboard
-          .writeText(href)
-          .then(() => alert("Copied to clipboard"))
-      })
+      id.textContent += ' (copy)';
+      id.className += ' cursor-pointer underline hover:text-blue-400';
+      id.addEventListener('click', () => {
+        const href = new URL(room, window.location).href;
+        navigator.clipboard.writeText(href).then(() => alert('Copied to clipboard'));
+      });
     }
-    loading.append(id)
-  })
-})
+    loading.append(id);
+  });
+});
 
-socket.on("ROOM_FULL", () => {
-  alert("Room already full!")
-  window.location = "/"
-})
+socket.on('ROOM_FULL', () => {
+  alert('Room already full!');
+  window.location = '/';
+});
 
-socket.on("DELETE_ROOM", () => (window.location = "/"))
+socket.on('DELETE_ROOM', () => (window.location = '/'));
 
-socket.on("START", startGame)
+socket.on('START', startGame);
 
-socket.on("PLAYER", (nextPlayer) => (player = nextPlayer))
+socket.on('PLAYER', (nextPlayer) => (player = nextPlayer));
 
-socket.on("UPDATE", render)
+socket.on('UPDATE', render);
 
 function startGame(nextState) {
-  loading.remove()
-  $("main").classList.remove("hidden")
-  $("main").classList.add("flex")
-  render(nextState)
+  loading.remove();
+  $('main').classList.remove('hidden');
+  $('main').classList.add('flex');
+  render(nextState);
 }
 
-let restartButton
+let restartButton;
 function render(state) {
-  if (state.status === "over") {
-    title.textContent = `Over! ${
-      state.winningPlayer
-        ? state.winningPlayer === player
-          ? "You"
-          : "You haven't"
-        : "Nobody"
-    } won!`
+  if (state.status === 'over') {
+    let msg = 'Nobody won.';
+    if (state.winningPlayer) {
+      msg = state.winningPlayer === player ? 'You won!' : 'You lost.';
+    }
+    title.textContent = msg;
 
-    restartButton = document.createElement("button")
-    restartButton.className =
-      "mx-auto mt-4 border-2 border-blue-400 rounded p-2 px-4 bg-blue-100"
-    restartButton.textContent = "Restart"
-    restartButton.addEventListener("click", () => socket.emit("RESTART"))
-    document.body.append(restartButton)
+    restartButton = document.createElement('button');
+    restartButton.className = 'mx-auto mt-4 border-2 border-blue-400 rounded p-2 px-4 bg-blue-100';
+    restartButton.textContent = 'Restart';
+    restartButton.addEventListener('click', () => socket.emit('RESTART'));
+    document.body.append(restartButton);
   } else {
-    if (restartButton) restartButton.remove()
-    title.textContent = "Tic Tac Toe"
+    if (restartButton) restartButton.remove();
+    title.textContent = 'Tic Tac Toe';
   }
 
-  noOverElements.forEach((el) =>
-    el.classList.toggle("hidden", state.status === "over"),
-  )
+  noOverElements.forEach((el) => el.classList.toggle('hidden', state.status === 'over'));
 
-  youAre.textContent = player
-  currentPlayer.textContent = state.player
+  youAre.innerHTML = `You play as <span class="font-bold">${player}</span>.`;
+  currentPlayer.textContent = player === state.player ? "It's your turn." : 'Wait for your turn...';
 
   for (const [index, button] of buttons.entries()) {
-    button.disabled = state.player !== player || state.tiles[index]
+    button.disabled = state.player !== player || state.tiles[index];
 
     if (state.tiles[index]) {
-      button.textContent = state.tiles[index]
+      button.textContent = state.tiles[index];
     } else {
-      button.textContent = ""
+      button.textContent = '';
     }
   }
 }
 
 function handleButtonClick(tileId) {
-  socket.emit("CLICK", tileId)
+  socket.emit('CLICK', tileId);
 }
 
 for (const [index, button] of buttons.entries()) {
   button.className =
-    "grid-row-3 grid-col-3 w-12 h-12 border border-black disabled:cursor-none disabled:bg-gray-200"
+    'grid-row-3 grid-col-3 w-12 h-12 border border-black disabled:cursor-none disabled:bg-gray-200';
 
-  button.addEventListener("click", () => handleButtonClick(index))
-  gameContainer.append(button)
+  button.addEventListener('click', () => handleButtonClick(index));
+  gameContainer.append(button);
 }
