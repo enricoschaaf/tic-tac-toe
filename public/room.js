e6fa8411a3d46b76a2516d683e55952ac3390020
Jonas Kuske <30421456+jonaskuske@users.noreply.github.com>
2020-11-26 05:44:49 +0100

    add types, rejoin on disconnect, refactor

--- public/room.js
+++ public/room.js
@@ -1,12 +1,22 @@
-import io from 'https://cdn.skypack.dev/socket.io-client/dist/socket.io.min'
+import _io from 'https://cdn.skypack.dev/socket.io-client/dist/socket.io.min'
 
-const $ = document.querySelector.bind(document)
+const io = /** @type {import('socket.io-client')} */ (/** @type {unknown}*/ (_io))
+
+/**
+ * @template {keyof HTMLElementTagNameMap} Tag
+ * @param {Tag} tag
+ * @param {Partial<HTMLElementTagNameMap[Tag]>} attrs
+ * @returns {HTMLElementTagNameMap[Tag]}
+ */
 const create = (tag, attrs = {}) => Object.assign(document.createElement(tag), attrs)
+
+/** @type {Document['querySelector']} */
+const $ = document.querySelector.bind(document)
 
 const socket = io()
 const roomId = window.location.pathname.slice(1)
 
-let player = null
+let player = /** @type {import('../server').Player} */ (null)
 
 document.title += ` (${roomId})`
 
@@ -17,6 +27,7 @@ const $player = $('#player')
 const $loading = $('#loading')
 const $currentPlayer = $('#current-player')
 const $title = $('#title')
+const $reconnect = document.body.appendChild(create('p', { className: 'text-center font-bold' }))
 const $buttons = [...Array(9)].map((_, index) => {
   return create('button', {
     className: `w-12 h-12 border border-black disabled:cursor-none disabled:bg-gray-200`,
@@ -27,40 +38,49 @@ const $buttons = [...Array(9)].map((_, index) => {
 $restartButton.addEventListener('click', () => socket.emit('RESTART_GAME'))
 $gameContainer.append(...$buttons)
 
-socket.on('connect', () => socket.emit('JOIN_ROOM', roomId))
+let prevId = /** @type {string} */ (null)
+
+socket.on('connect', () => socket.emit('JOIN_ROOM', roomId, prevId))
 
 socket.on('disconnect', () => {
-  alert('You are no longer connected to the game room.')
-  window.location = '/'
+  renderReconnect('offline')
+  renderButtons({ status: 'over', currentPlayer: 'x', tiles: [], winner: null })
 })
 
 socket.on('ROOM_FULL', () => {
-  alert('The game room is already full!')
-  window.location = '/'
+  alert('This game room is already full.')
+  window.location.href = '/'
 })
 
-socket.on('DELETE_ROOM', () => {
+socket.on('ROOM_CLOSED', () => {
   alert('Your opponent left the game!')
-  window.location = '/'
+  window.location.href = '/'
 })
 
-socket.on('PLAYER_ASSIGNED', (nextPlayer) => (player = nextPlayer))
+socket.on('PLAYER_ASSIGNED', (/** @type {import('../server').Player} */ nextPlayer) => {
+  player = nextPlayer
+})
 
-socket.on('UPDATE_STATE', (state) => {
-  window.state = state
+socket.on('UPDATE_STATE', (/** @type {import('../server').State} */ state) => {
+  // Once the game starts, store the socketId so that we can rejoin in case of a disconnect
+  if (state.status !== 'idle') prevId = socket.id
+
   renderMain(state)
   renderLoading(state)
+  renderReconnect(state.status)
   renderPlayer(state)
   renderCurrentPlayer(state)
   renderButtons(state)
   renderOver(state)
 })
 
+/** @param {import('../server').State} state */
 function renderMain({ status }) {
   $main.classList.toggle('hidden', status === 'idle')
   $main.classList.toggle('flex', status !== 'idle')
 }
 
+/** @param {import('../server').State} state */
 function renderLoading({ status }) {
   $loading.classList.toggle('hidden', status !== 'idle')
   if (status !== 'idle') return
@@ -76,7 +96,7 @@ function renderLoading({ status }) {
     id.textContent += ' (copy)'
     id.className += ' font-bold cursor-pointer underline hover:text-blue-400'
     id.addEventListener('click', () => {
-      const href = new URL(roomId, window.location).href
+      const { href } = new URL(roomId, window.location.href)
       navigator.clipboard.writeText(href).then(() => {
         id.textContent = id.textContent.replace('(copy)', '(copied ✔)')
       })
@@ -86,6 +106,19 @@ function renderLoading({ status }) {
   $loading.append(id)
 }
 
+/** @param {import('../server').State['status'] | 'offline'} status */
+function renderReconnect(status) {
+  switch (status) {
+    case 'offline':
+      return ($reconnect.textContent = 'Trying to reconnect...')
+    case 'reconnecting':
+      return ($reconnect.textContent = 'Waiting for your opponent to reconnect...')
+    default:
+      $reconnect.innerHTML = '&nbsp;'
+  }
+}
+
+/** @param {import('../server').State} state */
 function renderPlayer({ status }) {
   $player.parentElement.classList.toggle('hidden', status === 'over')
   if (status === 'over') return
@@ -93,6 +126,7 @@ function renderPlayer({ status }) {
   $player.textContent = player
 }
 
+/** @param {import('../server').State} state */
 function renderCurrentPlayer({ status, currentPlayer }) {
   $currentPlayer.classList.toggle('hidden', status === 'over')
   if (status === 'over') return
@@ -101,22 +135,20 @@ function renderCurrentPlayer({ status, currentPlayer }) {
   $currentPlayer.textContent = isCurrentPlayer ? "It's your turn." : 'Wait for your turn...'
 }
 
+/** @param {import('../server').State} state */
 function renderButtons({ status, currentPlayer, tiles }) {
   for (const [index, button] of $buttons.entries()) {
     const tileValue = tiles[index]
-    button.disabled = status !== 'started' || currentPlayer !== player || tileValue !== null
+    button.disabled = status !== 'active' || currentPlayer !== player || tileValue !== null
     button.textContent = tileValue || ''
   }
 }
 
-function renderOver({ status, winningPlayer }) {
+/** @param {import('../server').State} state */
+function renderOver({ status, winner }) {
   $restartButton.classList.toggle('hidden', status !== 'over')
 
-  let msg = 'Tic Tac Toe'
-
   if (status === 'over') {
-    msg = winningPlayer ? (winningPlayer === player ? 'You won!' : 'You lost.') : 'Nobody won.'
-  }
-
-  $title.textContent = msg
+    $title.textContent = winner ? (winner === player ? 'You won!' : 'You lost.') : 'Nobody won.'
+  } else $title.textContent = 'Tic Tac Toe'
 }
--- server.js
+++ server.js
@@ -3,87 +3,147 @@ import http from 'http'
 import path from 'path'
 import socketIo from 'socket.io'
 
-const app = express()
-const server = http.createServer(app)
-
 const PORT = process.env.PORT || 3000
 
+const app = express()
+const server = http.createServer(app)
+const io = socketIo(server, { pingInterval: 2000, pingTimeout: 1000 })
+
+app.use(express.static(path.resolve(__dirname, 'public')))
+app.get('/*', (req, res) => res.sendFile(path.resolve(__dirname, 'room.html')))
+
+const rooms = /** @type {Map<string, Room>} */ (new Map())
+
+/**
+ * @typedef {'x' | 'o'} Player
+ *
+ * @typedef {Object} State
+ * @prop {'idle' | 'active' | 'over' | 'reconnecting'} status
+ * @prop {Array<Player?>} tiles
+ * @prop {Player} currentPlayer
+ * @prop {Player?} winner
+ */
+
+/** @returns {State} */
 const getInitialState = () => ({
   status: 'idle',
+  winner: null,
   tiles: [...Array(9)].map(() => null),
   currentPlayer: Math.random() > 0.5 ? 'x' : 'o',
-  winningPlayer: null,
 })
 
 class Room {
-  playerIds = { x: null, o: null }
+  roomId = ''
+  players = /** @type {Map<string, Player>} */ (new Map())
   state = getInitialState()
+
+  constructor(/** @type {string} */ roomId) {
+    this.roomId = roomId
+  }
+
+  isCurrentPlayer(/** @type {string} */ id) {
+    return this.players.get(id) === this.state.currentPlayer
+  }
+
+  join(/** @type {string} */ id) {
+    const isFirst = this.players.size === 0
+    const player = isFirst ? 'x' : 'o'
+
+    this.players.set(id, player)
+    io.to(id).emit('PLAYER_ASSIGNED', player)
+
+    this.setState({ status: isFirst ? 'idle' : 'active' })
+  }
+
+  rejoin(/** @type {string} */ prevId, /** @type {string} */ id) {
+    const player = this.players.get(prevId)
+
+    this.players.delete(prevId)
+    this.players.set(id, player)
+
+    this.setState({ status: 'active' })
+  }
+
+  selectTile(/** @type {number} */ index) {
+    const tiles = [...this.state.tiles]
+    tiles[index] = this.state.currentPlayer
+
+    const winner = checkForWinner(tiles)
+
+    const isOver = Boolean(winner) || tiles.every((item) => item !== null)
+    const status = isOver ? 'over' : this.state.status
+
+    const nextPlayer = this.state.currentPlayer === 'x' ? 'o' : 'x'
+
+    this.setState({ status, tiles, winner, currentPlayer: nextPlayer })
+  }
+
+  setState(/** @type {Partial<State>} */ nextState) {
+    this.state = { ...this.state, ...nextState }
+
+    for (const [socketId] of this.players) {
+      io.to(socketId).emit('UPDATE_STATE', this.state)
+    }
+  }
+
+  reset() {
+    this.state = getInitialState()
+  }
 }
 
-const rooms = new Map()
+/** @param {string} roomId @param {string} socketId */
+function joinOrCreate(roomId, socketId) {
+  if (!rooms.has(roomId)) rooms.set(roomId, new Room(roomId))
 
-const io = socketIo(server)
+  const room = rooms.get(roomId)
+  if (room.state.status !== 'idle') throw 'ROOM_FULL'
 
-app.use(express.static(path.resolve(__dirname, 'public')))
+  room.join(socketId)
+}
 
-app.get('/:roomId', (_req, res) => {
-  res.sendFile(path.resolve(__dirname, 'room.html'))
-})
+/** @param {string} roomId @param {string} prevId @param {string} socketId */
+function rejoin(roomId, prevId, socketId) {
+  if (!rooms.has(roomId)) throw 'ROOM_CLOSED'
+
+  const room = rooms.get(roomId)
+
+  if (!room.players.has(prevId)) throw 'ROOM_CLOSED'
+
+  room.rejoin(prevId, socketId)
+}
 
 io.on('connection', (socket) => {
-  socket.on('JOIN_ROOM', (roomId) => {
-    if (!rooms.has(roomId)) {
-      socket.join(roomId)
-      const room = new Room()
-      socket.emit('PLAYER_ASSIGNED', 'x')
-      rooms.set(roomId, room)
-      room.playerIds.x = socket.id
-      io.to(roomId).emit('UPDATE_STATE', room.state)
-    } else {
-      const room = rooms.get(roomId)
-
-      if (room.state.status !== 'idle') {
-        return socket.emit('ROOM_FULL')
-      }
-
-      socket.join(roomId)
-      socket.emit('PLAYER_ASSIGNED', 'o')
-      room.playerIds.o = socket.id
-      room.state.status = 'started'
-      io.to(roomId).emit('UPDATE_STATE', room.state)
+  socket.on('JOIN_ROOM', (/** @type {string} */ roomId, /** @type {string?} */ prevId) => {
+    try {
+      if (prevId) rejoin(roomId, prevId, socket.id)
+      else joinOrCreate(roomId, socket.id)
+    } catch (errorEvent) {
+      socket.emit(errorEvent)
+      return
     }
 
+    const room = rooms.get(roomId)
+
     socket.on('disconnect', () => {
-      io.to(roomId).emit('DELETE_ROOM')
-      rooms.delete(roomId)
+      if (room.state.status === 'active') room.setState({ status: 'reconnecting' })
+      else {
+        for (const [id] of room.players) io.to(id).emit('ROOM_CLOSED')
+        rooms.delete(roomId)
+      }
     })
 
-    socket.on('TILE_CLICKED', (index) => {
-      const room = rooms.get(roomId)
-      const player = room.playerIds.x === socket.id ? 'x' : 'o'
+    socket.on('TILE_CLICKED', (/** @type {number} */ tileIndex) => {
+      if (room.state.status !== 'active') return
+      if (!room.isCurrentPlayer(socket.id)) return
 
-      if (player !== room.state.currentPlayer) return
-      if (room.state.status !== 'started') return
-
-      room.state.tiles[index] = player
-      room.state.currentPlayer = player === 'x' ? 'o' : 'x'
-
-      const [status, winningPlayer] = getStatusFromTiles(room.state.tiles)
-
-      room.state.status = status
-      room.state.winningPlayer = winningPlayer
-
-      io.to(roomId).emit('UPDATE_STATE', room.state)
+      room.selectTile(tileIndex)
     })
 
     socket.on('RESTART_GAME', () => {
-      const room = rooms.get(roomId)
+      if (room.state.status !== 'over') return
 
-      if (room.state.status === 'over') {
-        room.state = getInitialState()
-        room.state.status = 'started'
-        io.to(roomId).emit('UPDATE_STATE', room.state)
-      }
+      room.reset()
+      room.setState({ status: 'active' })
     })
   })
 })
@@ -99,19 +159,19 @@ const winningCombinations = [
   [2, 4, 6],
 ]
 
-function getStatusFromTiles(tiles) {
+/** @returns {Player?} */
+function checkForWinner(/** @type {State['tiles']} */ tiles) {
   for (const combination of winningCombinations) {
-    if (combination.every((index) => tiles[index] === 'x')) {
-      return ['over', 'x']
+    if (combination.every((i) => tiles[i] === 'x')) {
+      return 'x'
     }
-    if (combination.every((index) => tiles[index] === 'o')) {
-      return ['over', 'o']
+
+    if (combination.every((i) => tiles[i] === 'o')) {
+      return 'o'
     }
   }
 
-  if (tiles.every((item) => item !== null)) return ['over', null]
-
-  return ['started', null]
+  return null
 }
 
-server.listen(PORT, () => console.log(`Server is running on ${PORT}.`))
+server.listen(PORT, 0, () => console.log(`Server is running on ${PORT}.`))
